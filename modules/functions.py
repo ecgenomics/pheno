396cf3b6ff9f322cbd753ea2a53c2c982b0e011a
Fabio Barteri <72252439+ecgenomics@users.noreply.github.com>
2023-06-13 16:23:32 +0200

    towards the goodness
    dale

--- modules/functions.py
+++ modules/functions.py
@@ -42,10 +42,9 @@ def simOU(theta, mu, sigma, t, dt, s = 0):
 ### Simulates a Bwonian Motion process, starting from a
 ### known seed value (s = 0 by default)
 
-def simBM(t, dt, s=0):
+def simBM(t, dt, s):
 
     num_steps = int(t/dt)
-
     # White noise generation
     dW = np.random.normal(0, np.sqrt(dt), size=num_steps)
     dW = np.insert(dW,0, s, axis =0)
@@ -68,6 +67,13 @@ def cdist(tree, node1, node2):
     
     return distance
 
+### FUNCTION printdict()
+
+def printdict(dictionary, destination_path):
+    with open(destination_path, "w") as out:
+        for x in dictionary.keys():
+            print(x, dictionary[x], file = out)
+
 ### FUNCTION simulate_trait()
 ### simulates trait evolution on a tree
 
@@ -84,6 +90,23 @@ def simulate_trait(
 
                     ):
     
+    ### CLASS node_values()
+    ### Recaps all the simulation
+
+    class simulation():
+
+        def __init__(self):
+            self.tag = "sim_"
+            self.clades = []
+            self.n2n_paths = {}
+            self.node_values = {}
+            self.leaf_values = {}
+
+
+    ### CLASS node_values()
+    ### Adds simulation features to the Clade() class
+    ### Not derived from Clade(), but "parallel"
+
     class node_values():
 
         def __init__(self):
@@ -92,12 +115,18 @@ def simulate_trait(
 
             self.children = []
             self.terminals = []
-            self.leaves = []
-            self.path2child = {}
-            self.child_val = {}
+            self.terminal_values = []
+            self.terminal_values_mean = []
+            self.terminal_values_sd = []
 
+    # Read the tree
     tree = Phylo.read(input_tree,tree_format)
     
+
+    # Create the simulation() object
+
+    sim = simulation()
+
     # Initialize the tree (give a name to internal nodes int_#node)
 
     counter = 1
@@ -107,33 +136,65 @@ def simulate_trait(
             n.name = f"int_{counter}"
             counter += 1
     
-    # Collect data
-    
-    child_dict = {}
-    path_dict = {}
-    vals_dict = {}
-    
     # Iterate the simulation
+
+
     for clade in tree.find_clades(order='level'):
         z = node_values()
         z.name = clade.name
-
         try:
-            z.sim_val = z.vals_dict[z.name]
+            z.sim_val = sim.node_values[clade.name]
         except:
-            
             z.sim_val = seed
-
+            sim.node_values[z.name] = z.sim_val
+        
         if clade.clades:
-            z.children = [child.name for child in clade.clades]
-            child_dict[z.name] = [child.name for child in clade.clades]
+            z.children = [child for child in clade.clades]
+
+            for c in z.children:
+                c_t = clade.distance(c)
+                c_dt = st_dt
+                seed = z.sim_val
+
+                #### MODEL CHOICE ####
+
+                if mode.lower() == "bm":
+
+                    # BROWNIAN MOTION
+
+                    out = simBM(c_t,c_dt,seed)
+
+                elif mode.lower() == "ou":
+
+                    # ORNSTEIN-ULLEHNBECK
+
+                    out = simOU(mu = st_mu, sigma = st_sigma, theta = st_theta, t = c_t, dt = c_dt, s = seed)
+                
+                else:
+                    print("incorrect model")
+                    exit()
+
+                last_value = list(out)[-1]
+                sim.node_values[c.name] = last_value
+                sim.n2n_paths[c.name] = out
 
             z.terminals = [t.name for t in clade.get_terminals()]
-        
-        print(z.name, z.sim_val, z.children, z.terminals)
-
-        if clade.clades:
-            print(clade.distance(clade.clades[0]))
 
 
-    Phylo.draw(tree)
+        sim.clades.append(z)
+    
+    # Work on the terminals
+
+    for x in sim.clades:
+        if len(x.terminals) > 0: 
+            x.terminal_values = [sim.node_values[x] for x in x.terminals]
+            x.terminal_values_mean = np.mean(x.terminal_values)
+            x.terminal_values_sd = np.std(x.terminal_values)
+            print(x.name, len(x.terminals), x.terminals, x.terminal_values, x.terminal_values_mean, x.terminal_values_sd)
+
+
+
+    
+
+    printdict(sim.node_values, mode + "_node_values.txt")
+    printdict(sim.n2n_paths, mode + "_n2n_paths.txt")
--- pheno.py
+++ pheno.py
@@ -19,12 +19,23 @@ from modules.functions import *
 
 tree = Phylo.read("tree.nw", "newick")
 
-
 simulate_trait(
         
         input_tree = "tree.nw",
         mode = "bm"
                     )
+
+simulate_trait(
+        
+        input_tree = "tree.nw",
+        mode = "ou",
+        st_mu = 4,
+        st_dt = 0.1,
+        st_sigma = 0.002,
+        st_theta = 0.02
+                    )
+
+#print(simBM(1,0.1,4))
 
 ### End testing area
 
